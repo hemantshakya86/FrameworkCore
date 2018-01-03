9f61781a0f62cad9f331d434ff74221c0b736ec6
hemantshakya86 <hemant.shakya86@gmail.com>
2018-01-03 20:16:31 +0530

    second

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrameworkCore;
+using FrameworkCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication1.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public ValuesController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<ManageEvent> Get()
+        {
+            IRepository<ManageEvent> eventRepository = unitOfWork.Get<ManageEvent>();
+            IRepository<Country> coutryRepository = unitOfWork.Get<Country>();
+
+            var countries = coutryRepository.Query.Where(s => s.ID == 1).Select(s=>s.States).ToList();
+
+            //eventRepository.One(s => s.Name == "event");
+            //ManageEvent manageEvent = new ManageEvent();
+            //manageEvent.Name = "new event";
+            //manageEvent.Address = "update event";
+            //manageEvent.City = "new event";
+            //manageEvent.DasboardImage = "new event";
+            //manageEvent.EventEndDate = DateTime.Now;
+            //manageEvent.EventStartDate = DateTime.Now;
+            //manageEvent.EventManagerEmail = "new event";
+            //manageEvent.EventManagerName = "new event";
+            //manageEvent.LandingPageImage = "new event";
+            //manageEvent.Logo = "new event";
+            //eventRepository.Save(manageEvent);
+            //unitOfWork.Commit();
+            var eve = eventRepository.Query.ToList();
+
+            return eve;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using FrameworkCore;
+using FrameworkCore.Impl;
+using FrameworkCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        private IHostingEnvironment _hostingEnvironment;
+
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            _hostingEnvironment = env;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]);
+            });
+            // add identity
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+               
+            services.AddMvc();
+            services.AddScoped<IUnitOfWork, UnitOfWork>(); 
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
